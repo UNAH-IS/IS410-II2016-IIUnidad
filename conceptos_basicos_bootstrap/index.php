5e0f41dd48f5829addc6b44318dad5cdab307331
Erick Reyes <erick.marin.v@gmail.com>
2016-09-24 14:43:31 -0600

    Conceptos basico bootstrap

---
+++ conceptos_basicos_bootstrap/index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Oh no, Examen práctico</title> 
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/custom.css" rel="stylesheet">
+
+</head>
+<body>	
+	<div class="container-fluid">
+  		<div class="row">
+  			<div class="hidden-sm col-lg-4 col-md-3 col-sm-6 col-xs-6">
+  				<div class="well">
+  					Columna 1
+  					<div class="row">
+  						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+  							<div class="well">
+  								Subcolumna1
+  							</div>
+  						</div>
+  						<div class="col-lg-6  col-md-6 col-sm-6 col-xs-6">
+  							<div class="well">
+  								Subcolumna2
+  							</div>
+  						</div>
+  					</div>
+  				</div>
+  			</div>
+  			<div class="col-lg-2 col-md-3 col-sm-6 col-xs-6">
+  				<div class="well text-center">
+  					Columna 2
+  					<img src="img/icono2.png" class="img-responsive img-circle" alt="imagen  no encontrada">
+  				</div>
+  			</div>
+  			<div class="col-lg-2 col-md-3 col-sm-6 col-xs-6">
+  				<div class="well">
+  					Columna 3
+  				</div>
+  			</div>
+  			<div class="col-lg-2 col-md-3 col-sm-6 col-xs-6">
+  				<div class="well">
+  					Columna 4
+  				</div>
+  			</div>
+  			<div class="col-lg-2 col-md-3 col-sm-6 col-xs-6">
+  				<div class="well">
+  					Columna 5
+  				</div>
+  			</div>
+  			<div class="col-lg-2 col-md-6 col-sm-6 col-xs-6">
+  				<div class="well">
+  					Columna 6
+  				</div>
+  			</div>
+  			<div class="col-lg-2 col-md-3 col-sm-6 col-xs-6">
+  				<div class="well">
+  					Columna 7
+  				</div>
+  			</div>
+  			<div class="visible-xs-block col-lg-4 col-md-3 col-sm-6 col-xs-6">
+  				<div class="well">
+  					Columna 8
+  				</div>
+  			</div>
+  			<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+  				<div class="well">
+  					Columna 9
+  					<a class="btn btn-primary btn-lg" href ="http://google.hn" target="_blank">Ir a google</a><br>
+  					<a class="btn btn-danger btn-lg" href ="img/icono2.png" target="_blank" download>Descargar imagen</a><br>
+  					<span class="glyphicon glyphicon-thumbs-up">Like</span></a>
+  					<button type="button" class="btn btn-default btn-lg" aria-label="Left Align">
+						  <span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span>
+					</button>
+  				</div>
+  			</div>
+  		</div>
+  		<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+  			<div class="panel panel-danger">
+			  <div class="panel-heading">Este es el titulo</div>
+			  <div class="panel-body">
+			    	Este es el contenido
+			    	<ul class="nav nav-tabs">
+			    		<li role="presentation" class="active"><a href="#">Item 1</a></li>
+			    		<li role="presentation"><a href="#">Item 2</a></li>
+			    		<li role="presentation"><a href="#">Item 3</a></li>
+			    		<li role="presentation"><a href="http://google.hn">Item 4</a></li>
+			    	</ul>
+			    	<ul class="nav nav-pills">
+					  <li role="presentation" class="active"><a href="#">Home</a></li>
+					  <li role="presentation"><a href="#">Profile</a></li>
+					  <li role="presentation"><a href="#">Messages</a></li>
+					</ul>
+			  </div>
+			</div>
+  		</div>
+	</div>
+	<script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
