531290659d0b9a6fbbf6c8d9e37b897f654437c1
Erick Reyes <erick.marin.v@gmail.com>
2016-10-04 21:38:01 -0600

    Leer archivo, loading

--- ejemplo_basico_ajax/ajax/procesar.php
+++ ejemplo_basico_ajax/ajax/procesar.php
@@ -1,9 +1,25 @@
 <?php 
-	echo "Esta es la informacion que se recibe de index.php: <br>";
-	echo "Nombre de usuario: " . $_POST["txt-usuario"]."<br>";
-	echo "Contraseña: " . $_POST["txt-contrasena"]."<br>";
+	switch ($_GET["accion"]) {
+		case 'guardar':
+			sleep(10);
+			echo "Esta es la informacion que se recibe de index.php: <br>";
+			echo "Nombre de usuario: " . $_POST["txt-usuario"]."<br>";
+			echo "Contraseña: " . $_POST["txt-contrasena"]."<br>";
+			
+			$archivo = fopen("log_usuarios.csv","a"); //Nombre archivo, modo de apertura: r, w, a
+			fwrite($archivo,$_POST["txt-usuario"] . "," .$_POST["txt-contrasena"]."\n") ;
+			fclose($archivo);
+			break;
+		case 'obtener':
+			$archivo = fopen("log_usuarios.csv","r"); //Nombre archivo, modo de apertura: r, w, a
+			while($linea = fgets($archivo))
+				echo $linea."<br>";
 
-	$archivo = fopen("log_usuarios.csv","a"); //Nombre archivo, modo de apertura: r, w, a
-	fwrite($archivo,$_POST["txt-usuario"] . "," .$_POST["txt-contrasena"]."\n") ;
-	fclose($archivo);
+			fclose($archivo);
+			break;
+		default:
+			echo "Accion invalida";
+			break;
+	}
+	
 ?>
--- ejemplo_basico_ajax/index.php
+++ ejemplo_basico_ajax/index.php
@@ -8,26 +8,52 @@
 <body>
 	<input type = "text" id="txt-usuario" placeholder="Usuario" class="form-control"><br> 
 	<input type = "password" id="txt-contrasena" placeholder="Contraseña"  class="form-control"><br> 
-	<button id="btn-ingresar" class="btn btn-primary">Ingresar</button>
+	<button id="btn-ingresar" class="btn btn-primary"  data-loading-text="Guardando..." autocomplete="off">Ingresar</button>
+	<button id="btn-obtener-informacion" class="btn btn-danger"  data-loading-text="Obteniendo informacion..." autocomplete="off">Obtener informacion</button>
+	<img id="img-loading" src="img/loading.gif" style="display:none;">
 	<div id="div-respuesta" class="well"></div>
 	<script type="text/javascript" src="js/jquery.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
-	<script type="text/javascript">
+	<script>
 		$(document).ready(function(){
-			///alert("Estamos ready");
+			///alert("Estamos ready, el DOM fue cargado");
 			$("#btn-ingresar").click(function(){
+				$("#btn-ingresar").button("loading");
+				$("#img-loading").fadeIn(200);
 				var parametros = "txt-usuario="+$("#txt-usuario").val()+"&"+"txt-contrasena="+$("#txt-contrasena").val(); //formato similar a cuando se envia la informacion por GET
-				//URL?parametro1=valor1&parametro2=valor2&.....parametroN=valorN
+				//parametro1=valor1&parametro2=valor2&.....parametroN=valorN
 				alert("Informacion que se enviara: " + parametros);
 				$.ajax({
-					url:"ajax/procesar.php",
+					url:"ajax/procesar.php?accion=guardar",
 					method:"POST",
 					data: parametros,
 					success:function(respuesta){
+						$("#img-loading").fadeOut(200);
+						$("#btn-ingresar").button("reset");
 						$("#div-respuesta").html(respuesta);
+					},
+					error:function(){
+						alert("Ocurrio un error.");
 					}
 				});	
-			});			
+			});	
+
+			$("#btn-obtener-informacion").click(function(){
+				$("#btn-obtener-informacion").button("loading");
+				$("#img-loading").fadeIn(200);
+				$.ajax({
+					url:"ajax/procesar.php?accion=obtener",
+					method:"POST",
+					success:function(respuesta){
+						$("#img-loading").fadeOut(200);
+						$("#btn-obtener-informacion").button("reset");
+						$("#div-respuesta").html(respuesta);
+					},
+					error:function(){
+						alert("Ocurrio un error.");
+					}
+				});	
+			});
 		});
 	</script>
 </body>
